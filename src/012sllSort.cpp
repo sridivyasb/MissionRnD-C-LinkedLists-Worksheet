dc8a95f6e6f6390be4de869a93e0a3458c1544dd
sridivyasb <sridivyanagoju@gmail.com>
2015-12-30 01:09:37 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -1,5 +1,5 @@
 /*
-OVERVIEW:  Given a single linked list of 0s 1s and 2s ,Sort the SLL such that zeroes 
+OVERVIEW:  Given a single linked list of 0s 1s and 2s ,Sort the SLL such that zeroes
 will be followed by ones and then twos.
 
 
@@ -22,5 +22,25 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
-	
+    struct node* curr = head;
+
+	int count[3] = { 0 };
+
+	while (curr != NULL){
+		count[curr->data]++;
+		curr = curr->next;
+	}
+
+	int i, j;
+	curr = head;
+	for (i = 0; i < 3; i++){
+
+		for (j = count[i]; j>0; j--){
+			curr->data = i;
+			curr = curr->next;
+		}
+	}
+
+	return;
+
 }
